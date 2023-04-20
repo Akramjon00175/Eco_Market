258ae562bd2510bb044805c67b81a7d39eba62a3
Akramjon <akramjon00175@gmail.com>
2023-04-20 09:26:34 -0700

    Initial Commit

---
+++ Eco_Market/Eco_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eco_MarketApp.swift
+//  Eco_Market
+//
+//  Created by Akramjon on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Eco_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
